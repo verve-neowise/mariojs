0bef1127f323342b011988092d36e862bb85a2e8
Neowise <verve.neowise@gmail.com>
2022-02-17 15:12:38 +0500

    remove sql file

--- select.sql
+++
@@ -1,8 +0,0 @@
---Faqat bittasini olish
-SELECT provayder.tel FROM users INNER JOIN provayder ON users.provayder_id == provayder.id;
-
---Kopiroq
-SELECT provayder.tel, users.name FROM users INNER JOIN provayder ON users.provayder_id == provayder.id;
-
---Tahallus berish (nomini qisqartirish)
-SELECT provayder.tel as tel, users.name as name FROM users INNER JOIN provayder ON users.provayder_id == provayder.id;
